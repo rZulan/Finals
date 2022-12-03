52e909ae79683e590ad523f0576c9976e1d5e36e
rZulan <roi.zulan@gmail.com>
2022-12-03 12:28:43 +0800

    tutor pages

---
+++ core/dashboard/tutor/index.php
@@ -0,0 +1,46 @@
+<?php
+define('PAGE_NAME', 'Tutor');
+
+include('../../../main.php');
+include('../../../utils/checker.php');
+include('../../../connection/main.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=BRAND_NAME . ' | ' . PAGE_NAME?></title>
+</head>
+<body> 
+    <?php
+    include('../../../parts/nav.php');
+    ?>
+
+    <div class="side-bar">
+        <ul>
+            <a href="../../home.php">HOME</a>
+            <?php
+            if(isTutor($SQL_Handle, $_SESSION['user_id'])) {
+                echo "<a href=\"../tutor/index.php\">TUTOR</a>";
+            }
+            ?>
+            <a href="">PROFILE</a>
+            <a href="">ACCOUNT</a>
+            <a href="">LOGOUT</a>
+        </ul>
+    </div>
+
+    <div class="db-options">
+        <ul>
+            <li><a href="schedule.php">My Students</a></li>
+            <li><a href="stats.php">My Schedule</a></li>
+            <li><a href="stats.php">My Statistics</a></li>
+            <li><a href="profile.php">My Tutor Profile</a></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
--- core/dashboard/tutor/stats.php
+++ core/dashboard/tutor/stats.php
@@ -1,10 +1,9 @@
 <?php
-define('PAGE_NAME', 'Dashboard');
+define('PAGE_NAME', 'Tutor - Stats');
 
-include('../../main.php');
-include('../../utils/checker.php');
-include('../../connection/main.php');
-
+include('../../../main.php');
+include('../../../utils/checker.php');
+include('../../../connection/main.php');
 ?>
 
 <!DOCTYPE html>
@@ -15,17 +14,17 @@ include('../../connection/main.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=BRAND_NAME . ' | ' . PAGE_NAME?></title>
 </head>
-<body>
+<body> 
     <?php
-    include('../../parts/nav.php');
+    include('../../../parts/nav.php');
     ?>
 
     <div class="side-bar">
         <ul>
-            <a href="../home.php">HOME</a>
+            <a href="../../home.php">HOME</a>
             <?php
             if(isTutor($SQL_Handle, $_SESSION['user_id'])) {
-                echo "<a href=\"tutor/index.php\">TUTOR</a>";
+                echo "<a href=\"../tutor/index.php\">TUTOR</a>";
             }
             ?>
             <a href="">PROFILE</a>
@@ -33,5 +32,6 @@ include('../../connection/main.php');
             <a href="">LOGOUT</a>
         </ul>
     </div>
+
 </body>
 </html>
